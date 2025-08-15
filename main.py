d73a2d22512eb0c6e4561b6246146043313d8f7c
AlissonDuarte <alissonsilvamorimduarte@gmail.com>
2025-08-15 20:13:03 -0300

    main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from database import Base, sync_engine
 from routers import user_router, tag_router, post_router, notification_router
@@ -9,8 +10,20 @@ load_dotenv()
 
 Base.metadata.create_all(bind=sync_engine)
 
+
 app = FastAPI()
 
+origins = [
+    os.getenv("FRONTEND_URL", "http://192.168.1.50:3000"),
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(post_router.router, prefix="/api")
 app.include_router(user_router.router, prefix="/api")
 app.include_router(tag_router.router, prefix="/api")
